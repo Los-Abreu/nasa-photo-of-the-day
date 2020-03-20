5a83f333b8e8bfa4ac1349558c1d97b54673fd29
Carlos Abreu <c.abreu00@gmail.com>
2020-03-19 20:12:14 -0400

    Day2 Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./App.css";
 import NasaImg from "./components/NasaImg";
+import styled from "styled-components";
 
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  text-align: center;
+  color:white;
+`;
+
+const Wrapper = styled.section`
+  padding: 20px;
+  background:black;
+`;
 
 function App() {
   return (
@@ -10,12 +22,14 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p> */}
+      <Wrapper>
       <div className="App-header">
-      <h1>Astronomy Picture Of The Day</h1>
+      <Title>Astronomy Picture Of The Day</Title>
       </div>
       <div className="imgCard">
       <NasaImg />
       </div>
+      </Wrapper>
     </div>
   );
 }
--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NasaCard.css";
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import styled from "styled-components";
+
+const Heading = styled.h3`
+    font-size: 1.5rem;
+    text-align: center;
+    color: white;
+`;
 
 function NasaCard (props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return(
     <div className="Nasa-Card">
         <img className="apodImg" alt="NASA APOD" src={props.imgUrl}/>
         <div className="apodDesc">
-            <h3>{props.title}</h3>
-            <h4>{props.date}</h4>
-            <p>{props.explanation}</p>
+        <Heading>Date: {props.date}</Heading>
+            <Button color="info" onClick={toggle} style={{ marginBottom: '1rem'}}>{props.title}</Button>
+            <Collapse isOpen={isOpen}>
+                <Card style={{ backgroundColor: 'lightgrey'}}>
+                    <CardBody>
+                    {props.explanation}
+                    </CardBody>
+                </Card>
+                </Collapse>
         </div>
     </div>
 );
